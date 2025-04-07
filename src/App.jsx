c4a8bde57b4441c49bd9de610da58a66cd7bc85d
Ak0773 <120209368+Ak0773@users.noreply.github.com>
2025-04-07 19:10:35 +0300

    elements positions is ready

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,36 @@ function App() {
 
 	return (
 		<div className='flex items-center justify-center min-h-screen gap-[80px]  bg-gray-100 '>
-			<div className='flex items-start justify-center 	 bg-yellow-300 w-sm h-[500px] '>
+			<div className=' bg-yellow-300 w-sm h-[500px] flex-col text-center flex  items-center border-b-gray-700 '>
 				<img
 					src={viteLogo}
-					className='logo w-[100px] h-[100px]'
+					className='logo w-[100px] h-[100px] mb-[50px]'
 					alt='Vite logo'
 				/>
+				<h2 className='p-[30px] text-2xl'>
+					Lorem ipsum dolor sit amet consectetur
+				</h2>
 			</div>
-			<div className='flex align-baseline flex-column justify-column bg-gray-300 w-sm h-[500px] inset-shadow-indigo-500  '>
+			<div className='  bg-gray-300 w-sm h-[500px] flex-col text-center flex  items-center  '>
 				<img
 					src={viteLogo}
-					className='logo w-[100px] h-[100px]'
+					className='logo w-[100px] h-[100px]  mb-[50px] '
 					alt='Vite logo'
 				/>
-				<h1>Vite + React</h1>
+				<h1 className='p-[30px] text-2xl'>
+					Lorem ipsum dolor sit amet consectetur
+				</h1>
 			</div>
-			<div className='flex items-start justify-center bg-blue-300 w-sm h-[500px] '>
+			<div className=' bg-blue-300 w-sm h-[500px] flex-col text-center flex  items-center'>
 				<img
 					src={viteLogo}
-					className='logo w-[100px] h-[100px]'
+					className='logo w-[100px] h-[100px]  mb-[50px]'
 					alt='Vite logo'
 				/>
+				<h1 className='p-[30px] text-2xl'>
+					Lorem ipsum dolor sit amet consectetur
+				</h1>
+				<p></p>
 			</div>
 		</div>
 	)
